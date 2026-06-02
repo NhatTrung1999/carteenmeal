e9a1e1351b52755c62b2191bd2c80e7848863cad
Trung Nguyen <trungnguyen15051999@gmail.com>
2026-06-02 15:51:25 +0700

    'initial-project'

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import MainLayout from '@/layouts/MainLayout';
+import { createBrowserRouter } from 'react-router';
+
+export const router = createBrowserRouter([
+  //   { path: '/', Component: () => <h1>Home</h1> },
+  {
+    path: '/',
+    Component: () => <MainLayout />,
+    children: [
+      { path: '/', Component: () => <h1>Main Home</h1> },
+      { path: '/test', Component: () => <h1>Test</h1> },
+    ],
+  },
+  { path: '/login', Component: () => <h1>Login</h1> },
+  { path: '/permission', Component: () => <h1>Permission</h1> },
+  { path: '/logger', Component: () => <h1>Logger</h1> },
+]);
